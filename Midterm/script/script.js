b471f03cee45fef042e8195dd0fe5f29494ff3cd
Claudia Goyette <claudiagoyette0206@gmail.com>
2018-02-25 23:20:39 -0500

    Halfway done
    The pages are set and the link for all the buttons are working

---
+++ Midterm/script/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  // hide the button leading to   question 1
+  $('button1').hide();
+  // function when you click on the "take the quiz button"
+  $('button').on('click',function(){
+    $('start').hide();
+    $('img').hide();
+    $('button1').show();
+  });
+
+  $('page1_1').on('click',function(){
+
+  });
+
+
+
+});
