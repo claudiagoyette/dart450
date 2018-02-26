e899c63adea45c61c7f841540ccec3e048e18611
Claudia Goyette <claudiagoyette0206@gmail.com>
2018-02-26 03:26:51 -0500

    final version

--- Midterm_final/script/script.js
+++ Midterm_final/script/script.js
@@ -1,16 +1,75 @@
 $(document).ready(function () {
-  // hide the button leading to   question 1
-  //$('button1').hide();
-  // function when you click on the "take the quiz button"
-  //$('button').on('click',function(){
-  //  $('start').hide();
-  //  $('img').hide();
-  //  $('button1').show();
-//  });
 
+// function that allows to discover the hastags on the page : "I want to show the world who I am " type
+$("#text1").css("opacity", 0.0);
+     $("#text1").hover(function() {
+         $(this).animate({opacity: 1.0}, 500);
+     }, function() {
+         $(this).animate({opacity: 0.0}, 500);
+     });
 
+     $("#text2").css("opacity", 0.0);
+        $("#text2").hover(function() {
+            $(this).animate({opacity: 1.0}, 500);
+        }, function() {
+            $(this).animate({opacity: 0.0}, 500);
+        });
 
-//  });
+        $("#text3").css("opacity", 0.0);
+           $("#text3").hover(function() {
+               $(this).animate({opacity: 1.0}, 500);
+           }, function() {
+               $(this).animate({opacity: 0.0}, 500);
+           });
+
+           $("#text4").css("opacity", 0.0);
+              $("#text4").hover(function() {
+                  $(this).animate({opacity: 1.0}, 500);
+              }, function() {
+                  $(this).animate({opacity: 0.0}, 500);
+              });
+
+              $("#text5").css("opacity", 0.0);
+                 $("#text5").hover(function() {
+                     $(this).animate({opacity: 1.0}, 500);
+                 }, function() {
+                     $(this).animate({opacity: 0.0}, 500);
+                 });
+
+                 $("#text6").css("opacity", 0.0);
+                      $("#text6").hover(function() {
+                          $(this).animate({opacity: 1.0}, 500);
+                      }, function() {
+                          $(this).animate({opacity: 0.0}, 500);
+                      });
+
+                      $("#text7").css("opacity", 0.0);
+                         $("#text7").hover(function() {
+                             $(this).animate({opacity: 1.0}, 500);
+                         }, function() {
+                             $(this).animate({opacity: 0.0}, 500);
+                         });
+
+                         $("#text8").css("opacity", 0.0);
+                            $("#text8").hover(function() {
+                                $(this).animate({opacity: 1.0}, 500);
+                            }, function() {
+                                $(this).animate({opacity: 0.0}, 500);
+                            });
+
+                            $("#text9").css("opacity", 0.0);
+                               $("#text9").hover(function() {
+                                   $(this).animate({opacity: 1.0}, 500);
+                               }, function() {
+                                   $(this).animate({opacity: 0.0}, 500);
+                               });
+
+                               $("#text10").css("opacity", 0.0);
+                                  $("#text10").hover(function() {
+                                      $(this).animate({opacity: 1.0}, 500);
+                                  }, function() {
+                                      $(this).animate({opacity: 0.0}, 500);
+                                  });
 
 
 
