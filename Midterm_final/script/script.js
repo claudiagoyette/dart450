c39b54ae93a060d3a8336b2be7828ee7e54ae443
Claudia Goyette <claudiagoyette0206@gmail.com>
2018-03-04 19:35:59 -0500

    3/4 animation
    for now, 3 of the 4 animations are working

--- Midterm_final/script/script.js
+++ Midterm_final/script/script.js
@@ -1,77 +1,123 @@
 $(document).ready(function () {
 
-// function that allows to discover the hastags on the page : "I want to show the world who I am " type
-$("#text1").css("opacity", 0.0);
-     $("#text1").hover(function() {
-         $(this).animate({opacity: 1.0}, 500);
-     }, function() {
-         $(this).animate({opacity: 0.0}, 500);
-     });
 
-     $("#text2").css("opacity", 0.0);
-        $("#text2").hover(function() {
-            $(this).animate({opacity: 1.0}, 500);
-        }, function() {
-            $(this).animate({opacity: 0.0}, 500);
-        });
+//  ------  ANIMATION 4 ------
 
-        $("#text3").css("opacity", 0.0);
-           $("#text3").hover(function() {
-               $(this).animate({opacity: 1.0}, 500);
-           }, function() {
-               $(this).animate({opacity: 0.0}, 500);
-           });
+/*** I tried putting this animation at the bottom of the script.js page but it would only work
+when the code is placed like this !
 
-           $("#text4").css("opacity", 0.0);
-              $("#text4").hover(function() {
-                  $(this).animate({opacity: 1.0}, 500);
-              }, function() {
-                  $(this).animate({opacity: 0.0}, 500);
-              });
+/* function that makes the image at the bottom of the page fade in slowly on the page :
+ "I don't want to feel alone" type */
 
-              $("#text5").css("opacity", 0.0);
-                 $("#text5").hover(function() {
-                     $(this).animate({opacity: 1.0}, 500);
-                 }, function() {
-                     $(this).animate({opacity: 0.0}, 500);
-                 });
+ // make the image opacity to 0 at first
+ $("#puppies").css("opacity",0);
+ // function : scrolling down the page to trigger the animation
+ $(window).scroll(function () {
+   // calculate window position and scroll tracking variables
+   var windowHeight = $(window).height();
+   var windowScrollPosTop = $(window).scrollTop();
+   var windowScrollPosBottom = windowHeight + windowScrollPosTop;
+   var objectOffset = $("#puppies").offset();
+   var objectOffsetTop = objectOffset.top;
 
-                 $("#text6").css("opacity", 0.0);
-                      $("#text6").hover(function() {
-                          $(this).animate({opacity: 1.0}, 500);
-                      }, function() {
-                          $(this).animate({opacity: 0.0}, 500);
-                      });
+   // if the page has been scrolled far enough to reveal the image (start the animation)
+   if (windowScrollPosBottom > objectOffsetTop) {
+     $("#puppies").animate({"opacity":1},
+     6000);
+   }
 
-                      $("#text7").css("opacity", 0.0);
-                         $("#text7").hover(function() {
-                             $(this).animate({opacity: 1.0}, 500);
-                         }, function() {
-                             $(this).animate({opacity: 0.0}, 500);
-                         });
-
-                         $("#text8").css("opacity", 0.0);
-                            $("#text8").hover(function() {
-                                $(this).animate({opacity: 1.0}, 500);
-                            }, function() {
-                                $(this).animate({opacity: 0.0}, 500);
-                            });
-
-                            $("#text9").css("opacity", 0.0);
-                               $("#text9").hover(function() {
-                                   $(this).animate({opacity: 1.0}, 500);
-                               }, function() {
-                                   $(this).animate({opacity: 0.0}, 500);
-                               });
-
-                               $("#text10").css("opacity", 0.0);
-                                  $("#text10").hover(function() {
-                                      $(this).animate({opacity: 1.0}, 500);
-                                  }, function() {
-                                      $(this).animate({opacity: 0.0}, 500);
-                                  });
+ });
 
 
 
+//  ------  ANIMATION 2 ------
+  /* function that allows to discover the hastags on the page :
+  "I want to show the world who I am " type  */
 
-});
+  // hover animation : opacity from 0 to 1 when mouse hover text1
+  $("#text1").css("opacity", 0.0);
+  $("#text1").hover(function() {
+    $(this).animate({opacity: 1.0}, 200);
+  }, function() {
+    $(this).animate({opacity: 0.0}, 500);
+  });
+ // hover animation : opacity from 0 to 1 when mouse hover text2
+  $("#text2").css("opacity", 0.0);
+  $("#text2").hover(function() {
+    $(this).animate({opacity: 1.0}, 200);
+  }, function() {
+    $(this).animate({opacity: 0.0}, 500);
+  });
+// hover animation : opacity from 0 to 1 when mouse hover text3
+  $("#text3").css("opacity", 0.0);
+  $("#text3").hover(function() {
+    $(this).animate({opacity: 1.0}, 200);
+  }, function() {
+    $(this).animate({opacity: 0.0}, 500);
+  });
+// hover animation : opacity from 0 to 1 when mouse hover text4
+  $("#text4").css("opacity", 0.0);
+  $("#text4").hover(function() {
+    $(this).animate({opacity: 1.0}, 200);
+  }, function() {
+    $(this).animate({opacity: 0.0}, 500);
+  });
+// hover animation : opacity from 0 to 1 when mouse hover text5
+  $("#text5").css("opacity", 0.0);
+  $("#text5").hover(function() {
+    $(this).animate({opacity: 1.0}, 200);
+  }, function() {
+    $(this).animate({opacity: 0.0}, 500);
+  });
+// hover animation : opacity from 0 to 1 when mouse hover text6
+  $("#text6").css("opacity", 0.0);
+  $("#text6").hover(function() {
+    $(this).animate({opacity: 1.0}, 200);
+  }, function() {
+    $(this).animate({opacity: 0.0}, 500);
+  });
+// hover animation : opacity from 0 to 1 when mouse hover text7
+  $("#text7").css("opacity", 0.0);
+  $("#text7").hover(function() {
+    $(this).animate({opacity: 1.0}, 200);
+  }, function() {
+    $(this).animate({opacity: 0.0}, 500);
+  });
+// hover animation : opacity from 0 to 1 when mouse hover text8
+  $("#text8").css("opacity", 0.0);
+  $("#text8").hover(function() {
+    $(this).animate({opacity: 1.0}, 200);
+  }, function() {
+    $(this).animate({opacity: 0.0}, 500);
+  });
+// hover animation : opacity from 0 to 1 when mouse hover text9
+  $("#text9").css("opacity", 0.0);
+  $("#text9").hover(function() {
+    $(this).animate({opacity: 1.0}, 200);
+  }, function() {
+    $(this).animate({opacity: 0.0}, 500);
+  });
+// hover animation : opacity from 0 to 1 when mouse hover text10
+  $("#text10").css("opacity", 0.0);
+  $("#text10").hover(function() {
+    $(this).animate({opacity: 1.0}, 200);
+  }, function() {
+    $(this).animate({opacity: 0.0}, 500);
+  });
+
+
+
+//  ------  ANIMATION 3 ------
+ /* function that display the date/time appearing on the page:
+  "I believe everything I see on social medias" type */
+    // create a variable for the date
+    var date = new Date();
+    document.getElementById("today").innerHTML = date;
+
+
+
+
+
+
+
+  });
