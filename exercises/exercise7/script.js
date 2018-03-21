8ad7f50578d801265fb915a03644ac2016b54ae7
Claudia Goyette <claudiagoyette0206@gmail.com>
2018-03-21 09:50:26 -0400

    With comments

--- exercises/exercise7/script.js
+++ exercises/exercise7/script.js
@@ -6,48 +6,50 @@ Exercise 7
 */
 
 $(document).ready(function () {
-// animate the books from the top of the page
+  // animate the bookcover images from the top of the page, in a set amount of time
+  //Hunger Games
   $(function(){
-  		$("#hunger").animate({top:1000},5000)
-  	})
+    $("#hunger").animate({top:1000},10000)
+  })
+  // How to be the perfect Dutch
+  $(function(){
+    $("#bike").animate({top:1000},10000)
+  })
+  // Life of Pi
+  $(function(){
+    $("#life").animate({top:1000},10000)
+  })
+  // The Happy Lemon
+  $(function(){
+    $("#lemon").animate({top:1000},10000)
+  })
+// The Long Walk
+  $(function(){
+    $("#king").animate({top:1000},10000)
+  })
+// Harry Potter
+  $(function(){
+    $("#harry").animate({top:1000},10000)
+  })
+// Peter Pan
+  $(function(){
+    $("#peterpan").animate({top:1000},10000)
+  })
 
-    $(function(){
-        $("#bike").animate({top:1000},10000)
-      })
+  /* There is something wrong with my code, there is a gap between the mouse position and the
+  cart position, I will need help to fix it
+  */
 
-      $(function(){
-          $("#life").animate({top:1000},10000)
-        })
-
-        $(function(){
-            $("#lemon").animate({top:1000},10000)
-          })
-
-          $(function(){
-              $("#king").animate({top:1000},10000)
-            })
-
-            $(function(){
-                $("#harry").animate({top:1000},10000)
-              })
-
-              $(function(){
-                  $("#peterpan").animate({top:1000},10000)
-                })
-
-/* I tried here to make the image of the cart follow the mouse position
-but it didn't completely worked + the books were also follofing the position ...
-
-
+  // function so the cart follows the mouse position
   $ (document).mousemove (function (e) {
-  var x = e.pageX + 20;
-  var y = e.pageY + 20;
-
-   $("#cart").css('margin-left', x);
-   $("#cart").css('margin-top', y);
+    // variables to get the mouse x and y
+    var x = e.pageX;
+    //var y = e.pageY;
+    // get the cart position and make it the same as the mouse X and Y
+    $("#cart").css('left', x);
+    //$("#cart").css('top', y);
   });
 
 
-*/
 
 });
