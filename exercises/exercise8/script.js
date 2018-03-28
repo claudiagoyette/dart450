77b91191b6c515d7367fe995c64f57271321dfa0
Claudia Goyette <claudiagoyette0206@gmail.com>
2018-03-28 00:12:20 -0400

    Exercise 8

---
+++ exercises/exercise8/script.js
@@ -0,0 +1,73 @@
+/*
+
+Claudia Goyette
+Exercise 7
+
+*/
+
+$(document).ready(function () {
+  // animate the bookcover images from the top of the page, in a set amount of time
+  //Hunger Games
+  $(function(){
+    $("#hunger").animate({top:1000},10000)
+  })
+  // How to be the perfect Dutch
+  $(function(){
+    $("#bike").animate({top:1000},10000)
+  })
+  // Life of Pi
+  $(function(){
+    $("#life").animate({top:1000},10000)
+  })
+  // The Happy Lemon
+  $(function(){
+    $("#lemon").animate({top:1000},10000)
+  })
+// The Long Walk
+  $(function(){
+    $("#king").animate({top:1000},10000)
+  })
+// Harry Potter
+  $(function(){
+    $("#harry").animate({top:1000},10000)
+  })
+// Peter Pan
+  $(function(){
+    $("#peterpan").animate({top:1000},10000)
+  })
+
+  /* There is something wrong with my code, there is a gap between the mouse position and the
+  cart position, I will need help to fix it
+  */
+
+  // function so the cart follows the mouse position
+  $ (document).mousemove (function (e) {
+    // variables to get the mouse x and y
+    var x = e.pageX;
+    //var y = e.pageY;
+    // get the cart position and make it the same as the mouse X and Y
+    $("#cart").css('left', x);
+    //$("#cart").css('top', y);
+  });
+
+
+// set a counter for 2 minutes (the final version is not this short)
+// hide the times up message
+ $('#timesup').hide();
+ // create a counter for 2 minutes
+var counter = 10;
+var updateInterval = 1000;
+var timer = setInterval(function () { 
+   counter = counter - 1;
+   $('#counter').text(counter);
+   // if the counter = 0, make the counter stops, hide the cart and title
+   // make the times up message appear
+   if (counter == 0) {
+         $('#Title3').hide();
+         $('#cart').hide();
+         $('#timesup').show();
+         clearInterval(timer);
+       }
+ } , updateInterval);
+
+});
