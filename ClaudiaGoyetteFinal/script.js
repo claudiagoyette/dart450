9f28eb6390e569b627eddaf7ad6056f838c61e7d
Claudia Goyette <claudiagoyette0206@gmail.com>
2018-04-30 21:09:34 -0400

    Almost done
    Evertything works except one function. The commenting is all done on every page

--- ClaudiaGoyetteFinal/script.js
+++ ClaudiaGoyetteFinal/script.js
@@ -1,39 +1,60 @@
+// ************************************************************************
+
+
+//  Final by Claudia Goyette
+
+
+// ************************************************************************
+
+// create a variable for sound
+var clicSound;
+
 $(document).ready(function() {
+  // set the clic sound to be the mp3
+  clicSound = new Audio ( 'music/clic.mp3');
+
   // make the first image on the page "step 1" disappear so the second image is shown
   setTimeout('$("#step0").hide()',3600);
   // make the first image on the page "step 2" disappear so the second image is shown
   setTimeout('$("#step2").hide()',7000);
   // make the first image on the page "step 3" disappear so the second image is shown
   setTimeout('$("#step3").hide()',7000);
+  // make the first image on the page "laststep" disappear so the second image is shown
+  setTimeout('$("#laststep").hide()',7000);
 
-$('#title5').hide();
-$('.buttonStart1').hide();
+
+  // Using Annyang!
+  $('#title5').hide(); // hide the title before the voice is activated
+  $('.buttonStart1').hide(); // do the same for the button that starts the game
+  // Use this function so annyang is call when the page gets load
   window.onload = function(){
     if (annyang) {
       var commands = {
-        'Yes': function() {
+        'Yes': function() { // say yes to activate the voice
+          // use responsiveVoice
           responsiveVoice.speak("Thank you, you didn't have too. I need to finish my essay as soon as possible.I think you are the perfect candidate to help me.",
           "US English Male", {
           });
-          $('#teen').hide({
+          // when the user say yes, the page changes with a new menu
+          $('#teen').hide({   // hide the caracter
           });
-          $('#title1').hide({
+          $('#title1').hide({  // hide title1
           });
-          $('#title2').hide({
+          $('#title2').hide({  // hide title2
           });
-          $('#title3').hide({
+          $('#title3').hide({  // hide title3
           });
-          $('#title4').hide({
+          $('#title4').hide({  // hide title4
           });
-          $('#title5').show({
+          $('#title5').show({  // show title5
           });
-          $('.buttonStart1').show({
+          $('.buttonStart1').show({  // show th button that starts the game
           });
         }
       };
-
+      // receive commands
       annyang.addCommands(commands);
-
+      // start annyang
       annyang.start();
     }
   }
@@ -41,8 +62,9 @@ $('.buttonStart1').hide();
 
 
 
-  // ***  STEP 3 PAGE ***
+  // ***  STEP 2 PAGE ***
   $(function() {
+    // make elements sortable so the user can organize them into a sentence
     $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
   } );
@@ -50,28 +72,27 @@ $('.buttonStart1').hide();
 
 
   // ***  LAST STEP PAGE ***
-
-  // Make the things you want to drag draggable
+  // Make the pdf file image draggable
   $('#pdf').draggable();
-  // Make the element you want to drop onto "droppable"
-  // And if you want to have something happen when it is dropped
-  // then add an event handler function for 'drop' like this
+  // Make the class shares folder "droppable"
   $('#droponFolder').droppable({
     drop: handleDrop
   });
-
   // Called when the user drops something onto the droppable element
+  $("#ending").hide();  //hide caracter's image
   function handleDrop(event,ui) {
-    // You can get access to the thing that was dropped via ui.draggable
-    // So we'll remove it from the page
+    // remove the thing that was drop (pdf file) from the page
     ui.draggable.remove();
-    // when the player wins, remove everything from the screen
-    // and show the congradulation page
-    $("#mac").hide();
-    $("#folder").hide();
-    $("#droponFolder").hide();
-    $("#laststep").hide();
-    $(".buttonNext").hide();
+    // play the sound effect of a file dragged into a folder
+    clicSound.play();
+    // when the user wins, remove everything from the screen and show the congratulation page
+    $("#mac").hide();  // hide mac image
+    $("#folder").hide();  // hide folder image
+    $("#droponFolder").hide();  // hide the drop on folder zone image
+    $("#laststep").hide();  // hide caracter's image
+    $("#laststep_2").hide();  // hide caracter's image
+    $("#ending").show();  //show caracter's image
+    $(".buttonNext").hide();  // hide button
   }
 
 
@@ -101,7 +122,7 @@ $('.buttonStart1').hide();
 
 
   // ***  FIRST STEP PAGE ***
-
+  // use the key to control an object
   $(document).keydown(function(e)
   {
     var div = $("square");
@@ -141,52 +162,86 @@ $('.buttonStart1').hide();
 
 
 
+          // ***  RESPONSIVE VOICES ***
+          // caracter on the first page
           $('#teen').on('click',function(e) {
-           e.preventDefault();
-           responsiveVoice.speak("I need help, this semester is killing me", "US English Male", {rate: 1}, {volume: 1});
+            e.preventDefault();
+            // the text the responsive voice is saying is :
+            responsiveVoice.speak("I need help, this semester is killing me", "US English Male", {rate: 1}, {volume: 1});
           });
-          $('#step0').on('click',function(e) {
-           e.preventDefault();
-           responsiveVoice.speak("Thank you, you didn't have too", "US English Male", {rate: 1}, {volume: 1});
-          });
+          // caracter on the first step page
           $('#step1').on('click',function(e) {
-          e.preventDefault();
-          responsiveVoice.speak("Arrange the words as you like and press next when it's done", "US English Male", {rate: 1}, {volume: 1});
+            e.preventDefault();
+            // the text the responsive voice is saying is :
+            responsiveVoice.speak("Thank you, you didn't have too", "US English Male", {rate: 1}, {volume: 1});
           });
+          // caracter on the first step page but the second image
+          $('#step1_2').on('click',function(e) {
+            e.preventDefault();
+            // the text the responsive voice is saying is :
+            responsiveVoice.speak("Arrange the words as you like and press next when it's done", "US English Male", {rate: 1}, {volume: 1});
+          });
+          // caracter on second step the first page
           $('#step2').on('click',function(e) {
-           e.preventDefault();
-           responsiveVoice.speak("Good job! I also need to find a title, can you do that ?", "US English Male", {rate: 1}, {volume: 1});
-         });
-         $('#step2_2').on('click',function(e) {
-          e.preventDefault();
-          responsiveVoice.speak("Arrange the words as you like and press next when it's done", "US English Male", {rate: 1}, {volume: 1});
+            e.preventDefault();
+            // the text the responsive voice is saying is :
+            responsiveVoice.speak("Good job! I also need to find a title, can you do that ?", "US English Male", {rate: 1}, {volume: 1});
+          });
+          // caracter on the first page but the second image
+          $('#step2_2').on('click',function(e) {
+            e.preventDefault();
+            // the text the responsive voice is saying is :
+            responsiveVoice.speak("Arrange the words as you like and press next when it's done", "US English Male", {rate: 1}, {volume: 1});
+          });
+          // caracter on the first page
+          $('#step3').on('click',function(e) {
+            e.preventDefault();
+            // the text the responsive voice is saying is :
+            responsiveVoice.speak("You're good at this. Now can you help me correct my mistakes ?", "US English Male", {rate: 1}, {volume: 1});
+          });
+          // caracter on the first page but the second image
+          $('#step3_2').on('click',function(e) {
+            e.preventDefault();
+            // the text the responsive voice is saying is :
+            responsiveVoice.speak("Click on the words where you can see mistakes", "US English Male", {rate: 1}, {volume: 1});
+          });
+          // caracter on the last page
+          $('#laststep').on('click',function(e) {
+            e.preventDefault();
+            // the text the responsive voice is saying is :
+            responsiveVoice.speak("I think we are done! That was very nice of you.", "US English Male", {rate: 1}, {volume: 1});
+          });
+          // caracter on the last page but the second image
+          $('#laststep_2').on('click',function(e) {
+            e.preventDefault();
+            // the text the responsive voice is saying is :
+            responsiveVoice.speak("One last thing, can you put it in the class shares for me", "US English Male", {rate: 1}, {volume: 1});
+          });
+          // caracter on the last page but the ending page
+          $('#ending').on('click',function(e) {
+            e.preventDefault();
+            // the text the responsive voice is saying is :
+            responsiveVoice.speak("I don’t know how to thank you. You are the smartest and nicest human I know. I couldn’t have done it without you!! You saved my life.", "US English Male", {rate: 1}, {volume: 1});
+          });
+
+
+          // ***  STEP 3 PAGE ***
+          // use blast so when the user hover on a word that he thinks that has a mistake in it
+          $("div").blast({ search: "speach" }).mouseover(mistakes);  // select the word in the first line
+          $("div").blast({ search: "have" }).mouseover(mistakes);  // select the word in the line 2
+          $("div").blast({ search: "historicaly" }).mouseover(mistakes);  // select the word in the line 3
+          $("div").blast({ search: "question's" }).mouseover(mistakes);  // select the word in the line 4
+          $("div").blast({ search: "live" }).mouseover(mistakes);  // select the word in the line 5
+          $("div").blast({ search: "sujet" }).mouseover(mistakes);  // select the word in the line 6
+          // create a function for the hover : when the mouse is over a certain word, the word changes to blue
+          function mistakes () {
+            $(this).animate({
+              color: '#6bccf9', // color blue
+            });
+          }
+
+
+
+          // ending brakets
+
         });
-        $('#step3').on('click',function(e) {
-         e.preventDefault();
-         responsiveVoice.speak("You're good at this. Now can you help me correct my mistakes ?", "US English Male", {rate: 1}, {volume: 1});
-       });
-       $('#step3_2').on('click',function(e) {
-        e.preventDefault();
-        responsiveVoice.speak("Click on the words where you can see mistakes", "US English Male", {rate: 1}, {volume: 1});
-      });
-
-
-
-
-        });
-
-
-
-        //$("div").blast({ search: "subliminal" }).mouseover(reveal);
-        //  $("div").blast({ search: "evil" }).mouseover(reveal);
-        //  $("div").blast({ search: "ψ(｀∇´)ψ" }).mouseover(reveal);
-        //  $("div").blast({ search: "don't" }).mouseover(reveal);
-        //  $("div").blast({ search: "never" }).mouseover(reveal);
-        //  $("div").blast({ search: "not" }).mouseover(reveal);
-        //  $("div").blast({ search: "muahahahahaha!!!!" }).mouseover(reveal);
-
-        //function reveal () {
-        //  $(this).animate({
-        //      color: 'red',
-        //  });
-        //}
